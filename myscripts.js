252a6d9cd9581272e5e72f5a065571e9fb6dfa23
lucastoll <lucasfernandestolotto@gmail.com>
2021-10-28 10:09:49 -0300

    desenvolvimento 05
    desenvolvimento do site para mobile começado.
    
    header + primeiro slide feitos no mobile.

--- myscripts.js
+++ myscripts.js
@@ -2,3 +2,17 @@ $('.carousel').carousel({
     interval: 10000
     
   })
+
+/*   const mediaQuery = window.matchMedia('max-width: 600px')
+
+  function mobileslidechange(e){
+    let container = document.getElementById("containerslide"); 
+
+    if (e.matches) {
+    container.className = "container-fluid";
+    }
+  }
+
+
+mediaQuery.addListener(mobileslidechange)
+mobileslidechange(mediaQuery) */
