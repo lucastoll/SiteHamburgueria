e82b20820cdcc15ca8841dc63bcb52095a0c9146
lucastoll <lucasfernandestolotto@gmail.com>
2021-11-04 13:36:05 -0300

    desenvolvimento 09
    - pequenas correções de media querie
    - body com gradient (branco/vermelho)

--- myscripts.js
+++ myscripts.js
@@ -9,8 +9,6 @@ $('.play').carousel({
 
   /* A partir de 768 de width o container do primeiro slide vira container fluid*/
 
-
-  
   const mediaQuery = window.matchMedia('(max-width: 768px)')
 
   function mobileslidechange(e){
@@ -28,8 +26,6 @@ $('.play').carousel({
 
 mediaQuery.addListener(mobileslidechange)
 mobileslidechange(mediaQuery)
-
-
 
 /* A partir de 769 de width o container do primeiro slide é apenas container */
 
@@ -51,23 +47,3 @@ function mobileslidechange2(e){
 
 mediaQuery2.addListener(mobileslidechange2)
 mobileslidechange2(mediaQuery2)
-
-/* 
-const mediaQuery3 = window.matchMedia('(max-width: 576px)')
-
-function mobileslidechange3(e){
-  let container = document.getElementById("containerslide");
-  let container2 = document.getElementById("slidercardapio");
- 
-
-  if (e.matches) {
-  container.className = "container";
-  container2.className = "container";
-  }
-}
-
-
-mediaQuery3.addListener(mobileslidechange3)
-mobileslidechange3(mediaQuery3)
-
- */
